314b47996c42561a9ef4bf096ec77db5c835f546
httpini <nicolas.pinol@hotmail.com>
2022-06-14 17:46:04 -0300

    Se agregaron los formularios, pero sin estilos

--- source/index.js
+++ source/index.js
@@ -3,7 +3,9 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 1620
 
-app.listen(1620, ()=>console.log ("Abriendo el servidor http://localhost:4420"))
+app.listen(1620, ()=>console.log ("Abriendo el servidor http://localhost:1620"))
 const public = path.resolve (__dirname, "../public")
 app.use (express.static(public))
 app.get ("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views/home.html")))
+app.get ("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views/login.html")))
+app.get ("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views/register.html")))
