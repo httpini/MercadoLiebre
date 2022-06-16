5cbe044fa911d9f634ec31776933232cf722a105
httpini <nicolas.pinol@hotmail.com>
2022-06-16 01:56:46 -0300

    a

--- source/index.js
+++ source/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 1620;
 
-app.listen(1620, ()=>console.log ("Abriendo el servidor http://localhost:1620"))
+app.listen(process.env.PORT || 1620)
 const public = path.resolve (__dirname, "../public")
 app.use (express.static(public))
 app.get ("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views/home.html")))
